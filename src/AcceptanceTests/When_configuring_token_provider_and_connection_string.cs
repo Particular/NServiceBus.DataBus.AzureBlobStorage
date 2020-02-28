00ca6ca52cf72a862afac3cf8daa783fde4530bf
Sean Feldman <feldman.sean@gmail.com>
2020-02-28 15:16:51 -0700

    Verification test

---
+++ src/AcceptanceTests/When_configuring_token_provider_and_connection_string.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Auth;
+using NServiceBus;
+using NUnit.Framework;
+
+public class When_configuring_token_provider_and_connection_string
+{
+    [Test]
+    public void Should_throw()
+    {
+        var endpointConfiguration = new EndpointConfiguration("AzureBlobStorageDataBus.Test");
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.UseTransport<LearningTransport>();
+        endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        endpointConfiguration.EnableInstallers();
+
+        var dataBus = endpointConfiguration.UseDataBus<AzureDataBus>();
+        dataBus.ConnectionString("user-provided-connection-string");
+        dataBus.UseTokenCredentialForAccount(new TokenCredential(""), "user-provided-storage-account-name");
+
+        Assert.ThrowsAsync<Exception>(() => Endpoint.Start(endpointConfiguration));
+    }
+}
