0acea60b198081014101858ee9bccdaa6ee8dda6
Sean Feldman <feldman.sean@gmail.com>
2020-12-08 15:14:14 -0700

    Fix API Approvals

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -10,7 +10,10 @@
         [Test]
         public void Approve()
         {
-            var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureDataBus).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+            var publicApi = typeof(AzureDataBus).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+            });
             Approver.Verify(publicApi);
         }
     }
