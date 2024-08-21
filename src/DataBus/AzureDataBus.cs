f1dd7dcd42a522da655b1647d520012fe87e1e1a
williambza <william.brander@gmail.com>
2024-08-21 16:49:54 +0200

    Isolate azure databus

--- src/DataBus/AzureDataBus.cs
+++ src/DataBus/AzureDataBus.cs
@@ -2,21 +2,21 @@ namespace NServiceBus
 {
     using System;
     using DataBus;
-    using ClaimCheck.AzureBlobStorage;
+    using NServiceBus.DataBus.AzureBlobStorage;
 
     /// <summary>
     /// DataBus implementation that uses azure blob storage.
     /// </summary>
     [ObsoleteEx(Message = "AzureDataBus has been replaced by AzureClaimCheck.", RemoveInVersion = "8", TreatAsErrorFromVersion = "7", ReplacementTypeOrMember = "AzureClaimCheck")]
 #pragma warning disable CS0618 // Type or member is obsolete
-    public class AzureDataBus : DataBusDefinition
+    public class AaaaaaazureDataBus : DataBusDefinition
     {
         /// <summary>The feature to enable when this databus is selected.</summary>
 #pragma warning disable CS0672 // Member overrides obsolete member
         protected override Type ProvidedByFeature()
 #pragma warning restore CS0672 // Member overrides obsolete member
         {
-            return typeof(AzureClaimCheckPersistence);
+            return typeof(AzureDataBusPersistence);
         }
     }
 #pragma warning restore CS0618 // Type or member is obsolete
