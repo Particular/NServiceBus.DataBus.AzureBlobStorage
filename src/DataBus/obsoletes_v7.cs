a4f4568736d1938a60248ed0da73b011e65cc4cb
Tamara Rivera <tamita.rivera@gmail.com>
2025-06-24 16:15:10 -0700

    Prepare for NServiceBus 10 (#762)
    * Update workflow files
    
    * Fix CI workflow
    
    * Add workflows to the solution
    
    * Update the links to the docs
    
    * Bump to .NET 10.0
    
    * Bump NServiceBus packages
    
    * Clean up whitespace
    
    * Clean up obsoletes
    
    * Remove references to obsolete types
    
    * Add Obsoletes to deal with Fody problems
    
    * Update Fody
    
    * Update testing packages
    
    * Update Particular.Packaging
    
    * Update PublicApiGenerator
    
    * Update Azure.Storage.Blobs
    
    * Update API approval file
    
    * Migrate to slnx
    
    * Set LangVersion to preview to get 14.0
    
    ---------
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ src/DataBus/obsoletes_v7.cs
@@ -0,0 +1,65 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace NServiceBus.DataBus.AzureBlobStorage
+{
+    [ObsoleteEx(Message = "NServiceBus.DataBus.AzureBlobStorage.IProvideBlobServiceClient has been replaced by NServiceBus.ClaimCheck.AzureBlobStorage.IProvideBlobServiceClient.",
+        RemoveInVersion = "8",
+        TreatAsErrorFromVersion = "7",
+        ReplacementTypeOrMember = "NServiceBus.ClaimCheck.AzureBlobStorage.IProvideBlobServiceClient")]
+    public interface IProvideBlobServiceClient : ClaimCheck.AzureBlobStorage.IProvideBlobServiceClient
+    {
+    }
+}
+
+// Regular Obsolete attributes are needed here because Fody ObsoleteEx attributes are not considered an actual obsolete at compile time.
+// The obsolete messages have been duplicated to ensure that the correct message is seen by consumers of the package.
+// The ObsoleteEx attributes have been kept to ensure the build error happens when it's time to remove this file.
+namespace NServiceBus
+{
+    using System;
+    using Azure.Storage.Blobs;
+    using DataBus;
+
+    [ObsoleteEx(Message = "AzureDataBus has been replaced by AzureClaimCheck.",
+        RemoveInVersion = "8",
+        TreatAsErrorFromVersion = "7",
+        ReplacementTypeOrMember = "AzureClaimCheck")]
+    [DoNotWarnAboutObsoleteUsage]
+    [Obsolete("AzureDataBus has been replaced by AzureClaimCheck. Use `AzureClaimCheck` instead. Will be removed in version 8.0.0.", true)]
+    public class AzureDataBus : DataBusDefinition
+    {
+        [ObsoleteEx(Message = "AzureDataBus has been replaced by AzureClaimCheck.",
+            RemoveInVersion = "8",
+            TreatAsErrorFromVersion = "7",
+            ReplacementTypeOrMember = "AzureClaimCheck")]
+        [DoNotWarnAboutObsoleteUsage]
+        [Obsolete("AzureDataBus has been replaced by AzureClaimCheck. Use `AzureClaimCheck` instead. Will be removed in version 8.0.0.", true)]
+        protected override Type ProvidedByFeature() => throw new NotImplementedException();
+    }
+
+    [ObsoleteEx(Message = "AzureDataBus has been replaced by AzureClaimCheck. These extension methods are replaced by the ones on the AzureClaimCheck type.",
+        RemoveInVersion = "8",
+        TreatAsErrorFromVersion = "7",
+        ReplacementTypeOrMember = "AzureClaimCheck")]
+    [DoNotWarnAboutObsoleteUsage]
+    [Obsolete("AzureDataBus has been replaced by AzureClaimCheck. These extension methods are replaced by the ones on the AzureClaimCheck type. Use `AzureClaimCheck` instead. Will be removed in version 8.0.0.", true)]
+    public static class ConfigureAzureDataBus
+    {
+        public static DataBusExtensions<AzureDataBus> MaxRetries(this DataBusExtensions<AzureDataBus> config, int maxRetries) => throw new NotImplementedException();
+
+        public static DataBusExtensions<AzureDataBus> BackOffInterval(this DataBusExtensions<AzureDataBus> config, int backOffInterval) => throw new NotImplementedException();
+
+        public static DataBusExtensions<AzureDataBus> NumberOfIOThreads(this DataBusExtensions<AzureDataBus> config, int numberOfIOThreads) => throw new NotImplementedException();
+
+        public static DataBusExtensions<AzureDataBus> ConnectionString(this DataBusExtensions<AzureDataBus> config, string connectionString) => throw new NotImplementedException();
+
+        public static DataBusExtensions<AzureDataBus> Container(this DataBusExtensions<AzureDataBus> config, string containerName) => throw new NotImplementedException();
+
+        public static DataBusExtensions<AzureDataBus> BasePath(this DataBusExtensions<AzureDataBus> config, string basePath) => throw new NotImplementedException();
+
+
+        public static DataBusExtensions<AzureDataBus> UseBlobServiceClient(this DataBusExtensions<AzureDataBus> config, BlobServiceClient blobServiceClient) => throw new NotImplementedException();
+    }
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
